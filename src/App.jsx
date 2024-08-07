4402c82d367966747d9f3001ac80a8b59fa3d55e
David Erenfeld <1020dudu@gmail.com>
2024-08-07 12:04:40 +0300

    Fix navigation issues on Netlify deployment
    - Configured Vite to properly handle SPA routing in production.
    - Added a _redirects file to ensure all routes point back to index.html, resolving the 404 errors on refresh.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LocationChecker from "./Components/LocationChecker";
-import { filterStoresByCountry } from "./Utils/filterUtils";
-import { useStore } from "./contexts/storeContext";
-import useCountryCodes from "./Hooks/useCountryCodes";
 import "./App.css";
 import MainPage from "./Components/MainPage";
 
 const App = () => {
-  const { state, dispatch } = useStore();
-  const countryMap = useCountryCodes();
-
-  const handleCountrySelect = async (countryCode) => {
-    if (countryCode) {
-      const iso3Code = countryMap[countryCode]?.code3 || countryCode;
-      const filteredStores = await filterStoresByCountry(
-        state.stores,
-        iso3Code
-      );
-      dispatch({ type: "FILTER_STORES", payload: filteredStores });
-    } else {
-      dispatch({ type: "FILTER_STORES", payload: state.stores });
-    }
-  };
-
   return (
     <Router>
       <Routes>
